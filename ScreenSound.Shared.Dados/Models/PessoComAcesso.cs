33e64a14e350bb37bda7bb9f2ef0e46f6d67eb43
Alura Student <student@alura.com>
2026-06-02 00:08:44 -0300

    Integra Identity ao contexto e adiciona entidades de acesso
    Adicionado suporte ao ASP.NET Identity no ScreenSoundContext, criando as entidades PessoComAcesso e PerfilDeAcesso. Incluído o pacote Identity.EntityFrameworkCore e ajustes nos usings para integração de autenticação e autorização.

---
+++ ScreenSound.Shared.Dados/Models/PessoComAcesso.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScreenSound.Shared.Dados.Models
+{
+    public class PessoComAcesso : IdentityUser<int>
+    {
+    }
+}
